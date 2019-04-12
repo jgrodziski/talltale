32b7dac7e98fa72dedaa7b987dca7e3560d85b1a
jeremiegrodziski <jeremie.github@grodziski.com>
2019-04-12 12:35:32 +0100

    Add distribution management to project.clj for Clojars deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject talltale "0.3.0"
   :description "A CLJ/CLJS generator lib for fake data"
   :url "https://github.com/jgrodziski/talltale"
+  :deploy-repositories [["releases" :clojars]
+                         ["snapshots" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
